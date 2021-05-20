5498642b523fe13ce198d3668aba593842badd04
Shunn Pyae Pyae Aung <shunnpyaepyaeaung@Shunns-MacBook-Pro.local>
2021-05-20 22:41:08 +0630

    Revert "Gatsby SSR"
    This reverts commit 43e89dc32f5aa87baee7f3b77b9218cf25f79e16.

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import "../styles/global.scss";
-import "prismjs/themes/prism-tomorrow.css";
 
 const RootLayout = ({ children }) => {
   return <div className="theme-provider">{children}</div>;
