03633aa2468b114a72d998192947aead25efad68
Shunn Pyae Pyae Aung <shunnpyaepyaeaung@Shunns-MacBook-Pro.local>
2021-05-20 21:51:49 +0630

    Theme Toggle

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../styles/global.scss";
 import "prismjs/themes/prism-tomorrow.css";
+import "react-toggle/style.css";
 
 const RootLayout = ({ children }) => {
   return <div className="theme-provider">{children}</div>;
