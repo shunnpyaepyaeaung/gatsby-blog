f19d63fc0b51b5dc207cb0177223163832765361
Shunn Pyae Pyae Aung <shunnpyaepyaeaung@Shunns-MacBook-Pro.local>
2021-05-20 23:29:40 +0630

    Fontawesome icon

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import "../styles/global.scss";
 import "prismjs/themes/prism-tomorrow.css";
 import "react-toggle/style.css";
-import "./src/styles/global.scss";
-import "prismjs/themes/prism-tomorrow.css";
+import { library, config } from "@fortawesome/fontawesome-svg-core";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+
+config.autoAddCss = false;
+library.add(faSun, faMoon);
 
 const RootLayout = ({ children }) => {
   return <div className="theme-provider">{children}</div>;
--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import Toggle from "react-toggle";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ThemeToggle = ({ className }) => {
-  return <Toggle className={`day-night-toggle ${className}`} />;
+  return (
+    <Toggle
+      className={`day-night-toggle ${className}`}
+      icons={{
+        checked: <FontAwesomeIcon inverse icon="sun" />,
+        unchecked: <FontAwesomeIcon inverse icon="moon" />,
+      }}
+    />
+  );
 };
 
 export default ThemeToggle;
