43e89dc32f5aa87baee7f3b77b9218cf25f79e16
Shunn Pyae Pyae Aung <shunnpyaepyaeaung@Shunns-MacBook-Pro.local>
2021-05-20 15:53:24 +0630

    Gatsby SSR

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "../styles/global.scss";
+import "prismjs/themes/prism-tomorrow.css";
 
 const RootLayout = ({ children }) => {
   return <div className="theme-provider">{children}</div>;
