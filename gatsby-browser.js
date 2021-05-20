2c22104132834b1c47a4d099285fd2a95a43f6d7
Shunn Pyae Pyae Aung <shunnpyaepyaeaung@Shunns-MacBook-Pro.local>
2021-05-20 15:37:57 +0630

    fixed wrapRootElement

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,4 +3,6 @@ import "prismjs/themes/prism-tomorrow.css";
 import React from "react";
 import RootLayout from "./src/components/RootLayout";
 
-export const wrapRootElement = ({ element }) => <RootLayout />;
+export const wrapRootElement = ({ element }) => (
+  <RootLayout>{element}</RootLayout>
+);
